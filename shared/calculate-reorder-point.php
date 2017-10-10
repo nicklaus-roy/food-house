77584e0f938e6451e6c1ce1d8078e191c62cafc1
Nicklaus Roy <roynicklaus@gmail.com>
2017-10-10 15:35:54 +0800

    Fixed It

--- shared/calculate-reorder-point.php
+++ shared/calculate-reorder-point.php
@@ -1,5 +1,5 @@
 <?php
-    $conn = mysqli_connect('localhost', 'root', 'slsc', 'foodjectives');
+    $conn = mysqli_connect('localhost', 'root', 'root', 'foodjectives');
     if(mysqli_connect_errno()){
         die("Failed to connect :".mysqli_connect_error());
     }
@@ -9,10 +9,11 @@
         LEFT JOIN stock_issuances si on si.raw_material_id = r.id
         WHERE si.date_issued >= '2017-09-08' AND si.date_issued <= '2017-10-08'
         AND r.id = '$raw_material_id'")->fetch_assoc();
-    
-        $reorder_quantity = $reorder_details['reorder_quantity'];
-        $reorder_point = $reorder_details['reorder_point'];
+
+        $reorder_quantity = $reorder_points['reorder_qty'];
+        $reorder_point = $reorder_points['reorder_point'];
+        
 
         $conn->query("UPDATE raw_materials SET reorder_point = '$reorder_point',
             reorder_qty = '$reorder_quantity' WHERE id = '$raw_material_id'");
-?>
+?>  
